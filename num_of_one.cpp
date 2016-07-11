e2b71aa5c5cfbcabab0dfe79143c77847594d9c7
liyaxu <liyaxu19871006@gmail.com>
2016-07-11 23:32:45 +0800

    fix count num of one if negtive numbers

--- num_of_one.cpp
+++ num_of_one.cpp
@@ -3,16 +3,30 @@
 
 int num_of_one(int num) {
     int total = 0;
+    int move_num = 0;
     while (num != 0) {
+        if (move_num == 32)  break;
         if (num & 1)  ++total;
         num >>= 1;
+        move_num++;
     }
     return total;
+}
+int count_one(int num) {
+    int x = num;
+    x = (x & (0x55555555)) + ((x >> 1) & (0x55555555));
+    x = (x & (0x33333333)) + ((x >> 2) & (0x33333333));
+    x = (x & (0x0f0f0f0f)) + ((x >> 4) & (0x0f0f0f0f));
+    x = (x & (0x00ff00ff)) + ((x >> 8) & (0x00ff00ff));
+    x = (x & (0x0000ffff)) + ((x >> 16) & (0x0000ffff));
+    return x;
 }
 
 int main() {
     printf("num_of_one(%d) = %d\n", 10, num_of_one(10));
     printf("num_of_one(%d) = %d\n", 0, num_of_one(0));
     printf("num_of_one(%d) = %d\n", -10, num_of_one(-10));
+    printf("num_of_one(%d) = %d\n", -10, count_one(-10));
+    printf("num_of_one(%d) = %d\n", 10, count_one(10));
     return 0;
 }
